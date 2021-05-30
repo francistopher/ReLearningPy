7e292adcf233fced0c35cf6751017aac8e28e5cb
francistopher <francistopherr@gmail.com>
2021-05-30 00:28:18 -0400

    UPDATE Nested List Comprehensions Lessons to include del statement

--- 05/9nested_list_comprehensions_del.py
+++ 05/9nested_list_comprehensions_del.py
@@ -61,3 +61,27 @@ print(matrix) # output below
 
 print("\n# If we break the inner list comprehension into a for loop")
 print("# We get back to the original nested for loops, back to square 1")
+
+print("\n# Remember when we used pop() to return and remove the last value in a list")
+print("# Remember when we used remove(index) to remove the value at that index in a list")
+print("# There's another way, using the del statement, del list_name[slice]")
+
+print("""
+del matrix[0][2] # from the first list in the matrix, delete the 3rd element
+
+print(matrix) # output below
+""")
+
+del matrix[0][2]
+print(matrix)
+
+print("\n# Not only can we remove elements one at a time, we can slice them out of the list!")
+
+print("""
+del matrix[1][1:3] # from the second list in the matrix delete the last 2 elements
+
+print(matrix) # output below
+""")
+
+del matrix[1][1:3]
+print(matrix)
